4291e4582a3c3ac67bf9ca68da70331c02bdb873
muhammadAgfian96 <muhammadagfian96@gmail.com>
2019-05-07 14:41:35 +0700

    coba function

--- 002 (tipe data)/function.js
+++ 002 (tipe data)/function.js
@@ -1,11 +1,13 @@
-var a =8;
-var b =3;
-var volumeA;
-var volumeB;
-var volumeC;
-var total;
+var hihi;
+function hitung_volume(a,b){
+    var volumeA, volumeB, total;
 
-volumeA = a * a * a;
-volumeB = b * b * b;
+    volumeA = a*a*a;
+    volumeB = b*b*b;
 
-total = volumeA + volumeB;
+    total = volumeA+volumeB;
+    return total;
+}
+
+hihi = hitung_volume(4,4);
+console.log(hihi);
