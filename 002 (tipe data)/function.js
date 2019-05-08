2a01789d4d780548d30d04c099c369c1b0f96564
muhammadAgfian96 <muhammadagfian96@gmail.com>
2019-05-08 10:56:46 +0700

    hehehe udah sampe array

--- 002 (tipe data)/function.js
+++ 002 (tipe data)/function.js
@@ -1,4 +1,5 @@
 var hihi;
+
 function hitung_volume(a,b){
     var volumeA, volumeB, total;
 
@@ -11,3 +12,22 @@ function hitung_volume(a,b){
 
 hihi = hitung_volume(4,4);
 console.log(hihi);
+
+var arr = ['lia', 'sul', 'osy', 'bayu', 'farid','wicak'];
+
+arr.splice(2,0,'wkwkwk', 'hahah')
+console.log(arr.join(' - '))
+
+var arr2 = ['apasih', 'belajar', 'yang', 'bener'];
+var arr3 = arr2.slice(1,3);
+console.log(arr3.join('-' ))
+
+//6. for eadch
+angka = [1,2,3,4,5,6,7,8,9]
+
+var cetak = function(e,i){
+    console.log('Mahasiswa ke-'+i+ ' adalah: '+e);
+    // console.log(e);
+}
+
+arr.forEach(cetak);
